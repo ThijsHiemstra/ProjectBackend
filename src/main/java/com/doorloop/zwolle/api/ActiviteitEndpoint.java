94bf6b5f2413bd9f825b08043cfccf4276288bb1
SijbrenSchotanus <sts1990@hotmail.com>
2019-10-22 11:45:04 +0200

    hoi

--- src/main/java/com/doorloop/zwolle/api/ActiviteitEndpoint.java
+++ src/main/java/com/doorloop/zwolle/api/ActiviteitEndpoint.java
@@ -4,29 +4,33 @@ import com.doorloop.zwolle.domain.Activiteit;
 import com.doorloop.zwolle.persistence.ActiviteitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActiviteitEndpoint {
+
     @Autowired
     ActiviteitService activiteitService;
 
-@GetMapping("/Activiteit")
+    @GetMapping("/activiteit")
     public Activiteit workout1(){
-    System.out.println("Welcome to workout #1");
-    Activiteit Act1 = new Activiteit();
-    Act1.setTypeOfExercise(null);
-    Act1.setCalPerHour(0);
-    Act1.setDurationOfExercise(0);
-    Act1.setSuccess(false);
-    Act1.setTimeIfFail(0);
-    Act1.setCalBurned(0);
+        System.out.println("Welcome to workout #1");
+        Activiteit Act1 = new Activiteit();
+        Act1.setTypeOfExercise(null);
+        Act1.setCalPerHour(0);
+        Act1.setDurationOfExercise(0);
+        Act1.setSuccess(false);
+        Act1.setTimeIfFail(0);
+        Act1.setCalBurned(0);
+        return  Act1;
+    }
 
-    return  Act1;
-
-
-}
-
-
-
+    @PostMapping("/activiteit")
+    public void saveActiviteit(@RequestBody Activiteit activiteit){
+        System.out.println(activiteit.getCalPerHour());
+        activiteitService.saveActiviteit(activiteit);
+        System.out.println();
+    }
 }
--- src/main/java/com/doorloop/zwolle/persistence/ActiviteitService.java
+++ src/main/java/com/doorloop/zwolle/persistence/ActiviteitService.java
@@ -8,4 +8,25 @@ import org.springframework.stereotype.Service;
 public class ActiviteitService {
     @Autowired
     ActiviteitRepository activitieitRepository;
+
+
+
+    public Iterable<Activiteit> geefMeAlleActiviteiten(){
+
+        Activiteit Act1 = new Activiteit();
+        Act1.setTypeOfExercise(null);
+        Act1.setCalPerHour(0);
+        Act1.setDurationOfExercise(0);
+        Act1.setSuccess(false);
+        Act1.setTimeIfFail(0);
+        Act1.setCalBurned(0);
+
+        return activitieitRepository.findAll();
+
+    }
+
+    public void saveActiviteit(Activiteit activiteit){
+        activitieitRepository.save(activiteit);
+    }
+
 }
